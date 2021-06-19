86a8d5daa11259e45ec04d1444487214a684c924
SUMANTH <54896751+sumanth13131@users.noreply.github.com>
2021-06-19 08:54:15 +0530

    Update app.py
    threshold added

--- app.py
+++ app.py
@@ -12,8 +12,15 @@ def home():
 def output():
     bs64string = request.get_json()
     result = Helper().predict(bs64string)
+    if(result['acc'] > 60):
+        res = make_response(jsonify(result),200)
+        return res
+    
+    result['cls'] = 'Low Accuracy'
     res = make_response(jsonify(result),200)
     return res
+    
+    
 
 
 if __name__ == '__main__':
